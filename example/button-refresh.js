14308b6d95c991c9f1fd2e390f09e6b29d2e732b
mediocreatmybest <80406625+mediocreatmybest@users.noreply.github.com>
2021-07-09 20:13:42 +0930

    Update button-refresh.js

--- example/button-refresh.js
+++ example/button-refresh.js
@@ -1,4 +1,4 @@
-$("#refresh-button").click(function() {
+$("#refresh-button").onClick(function() {
     alert('click');
     $("#main-container").load("#main-container .headliner .headliner-url > *");
   }); 
