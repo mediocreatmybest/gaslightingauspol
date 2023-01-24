c871bb2fa99ddf177406d4a49af6fbdf87e7dc7e
mediocreatmybest <80406625+mediocreatmybest@users.noreply.github.com>
2023-01-24 15:11:54 +0930

    more tests

---
+++ example/rotate-script.js
@@ -0,0 +1,10 @@
+document.getElementById("random-btn").addEventListener("click", function(){
+    randomHeadliner();
+    var element = document.getElementById("headliner");
+    element.classList.remove("rotate-back");
+    element.classList.add("rotate");
+    setTimeout(function(){
+      element.classList.remove("rotate");
+      element.classList.add("rotate-back");
+    }, 1000);
+});
