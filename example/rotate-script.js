65fdabc94321f019c8459f2fa1fae493c6a9f99e
mediocreatmybest <80406625+mediocreatmybest@users.noreply.github.com>
2023-09-09 23:31:25 +0930

    animation fix

--- example/rotate-script.js
+++ example/rotate-script.js
@@ -1,10 +1,13 @@
 document.getElementById("random-btn").addEventListener("click", function(){
-    randomHeadliner();
-    var element = document.getElementById("rotate-block");
-    element.classList.remove("rotate-back");
-    element.classList.add("rotate");
-    setTimeout(function(){
+  var element = document.getElementById("rotate-block");
+  element.classList.remove("rotate-back");
+  element.classList.add("rotate");
+  setTimeout(function(){
+      // Update the content halfway
+      randomHeadliner();
+  }, 500); // Halfway mark
+  setTimeout(function(){
       element.classList.remove("rotate");
       element.classList.add("rotate-back");
-    }, 1000);
+  }, 1000);
 });
